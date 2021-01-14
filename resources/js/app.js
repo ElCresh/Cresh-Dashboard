9c45ffcb2e71bf1315f1375eb71bc86a2b10ee8d
Andrea Crescentini <ac@andreacrescentini.com>
2021-01-14 23:40:03 +0100

    Added basic Material Dashboard template

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 require('./bootstrap');
+
+// Material Dashboard
+require('../../node_modules/material-dashboard/assets/js/core/bootstrap-material-design.min.js');
+require('../../node_modules/material-dashboard/assets/js/plugins/perfect-scrollbar.jquery.min.js');
+require('../../node_modules/material-dashboard/assets/js/material-dashboard.js');
---
+++ resources/views/layouts/page_templates/dashboard.blade.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>{{ config('app.name') }}</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+        <!--     Fonts and icons     -->
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+
+        <!-- Material Dashboard CSS -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="sidebar" data-color="orange" data-background-color="red" data-image="">
+                <div class="logo"><a href="/" class="simple-text logo-normal">
+                    {{ config('app.name') }}
+                </a></div>
+                <div class="sidebar-wrapper">
+                    <ul class="nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/">
+                                <i class="material-icons">dashboard</i>
+                                <p>Dashboard</p>
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link" href="#">
+                                <i class="material-icons">person</i>
+                                <p>_</p>
+                            </a>
+                        </li>
+                        <li class="nav-item active-pro ">
+                            <a class="nav-link" href="http://andreacrescentini.com" target="_blank">
+                                <i class="material-icons">alternate_email</i>
+                                <p>Andrea Crescentini</p>
+                            </a>
+                        </li>
+                    </ul>
+            </div>
+        </div>
+        <div class="main-panel">
+            <!-- Navbar -->
+            <nav class="navbar sticky-top navbar-expand-lg navbar-transparent">
+                <div class="container-fluid">
+                    <div class="navbar-wrapper">
+                        <a class="navbar-brand" href="javascript:;">Dashboard</a>
+                    </div>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="navbar-toggler-icon icon-bar"></span>
+                        <span class="navbar-toggler-icon icon-bar"></span>
+                        <span class="navbar-toggler-icon icon-bar"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-end">
+                        {{-- Noting here --}}
+                    </div>
+                </div>
+            </nav>
+            <!-- End Navbar -->
+            <div class="container">
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+
+        <!--   Core JS Files   -->
+        <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+        <script>
+            $('.sidebar .sidebar-wrapper, .main-panel').perfectScrollbar();
+            $('.main-panel').perfectScrollbar('destroy');
+            $('.main-panel').perfectScrollbar('update');
+        </script>
+    </body>
+</html>
